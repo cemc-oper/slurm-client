821f4a68fd96acb68839cd986688f1a9538f3082
perillaroc <perillaroc@gmail.com>
2018-12-27 14:35:31 +0800

    :heavy_plus_sign: check account for slurm long time job filter. fix #12

--- slurm_client/plugins/filters/long_time_job_filter.py
+++ slurm_client/plugins/filters/long_time_job_filter.py
@@ -1,9 +1,10 @@
 # coding=utf-8
 import datetime
-from nwpc_hpc_model.workload.filter_condition import \
-    PropertyFilterCondition, \
-    create_less_value_checker, \
-    get_property_data
+from nwpc_hpc_model.workload.filter_condition import (
+    PropertyFilterCondition,
+    create_less_value_checker,
+    create_in_value_checker,
+    get_property_data)
 from nwpc_hpc_model.workload.filter import Filter
 
 
@@ -18,7 +19,12 @@ def create_filter():
         data_checker=create_less_value_checker(datetime.datetime.utcnow()-datetime.timedelta(hours=5)),
         data_parser=get_property_data
     )
+    owner_condition = PropertyFilterCondition(
+        "squeue.account",
+        data_checker=create_in_value_checker(["nwp", "nwp_qu", "nwp_pd", "nwp_sp"])
+    )
     a_filter = Filter()
+    a_filter.conditions.append(owner_condition)
     a_filter.conditions.append(query_date_condition)
     return {
         'name': filter_info['name'],
